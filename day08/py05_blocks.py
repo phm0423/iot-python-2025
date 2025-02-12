d5e260b41465190d26713f2444967abaa1ab423c
phm0423 <qkrgusalsok@gmail.com>
2025-02-12 15:09:11 +0900

    Update py05_blocks.py

--- day08/py05_blocks.py
+++ day08/py05_blocks.py
@@ -10,6 +10,25 @@ import math
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 
+class Block:
+    def __init__(self, col, rect, speed = 0):
+        self.col = col
+        self.rect= rect
+        self.speed = speed
+        self.dir = random.randint(-45, 45) + 270    # 3+270
+
+    def move(self): # 볼 무브
+        # 볼의 움직이는 x축값을 계속 계산하려면 x은 dir 값을 라디언으로 변환 후 코사인 처리
+        self.rect.centerx += math.cos(math.radians(self.dir)) * self.speed
+        # 볼의 움직이는 y축값을 계속 계산하려면 y은 dir 값을 라디언으로 변환 후 사인 처리
+        self.rect.centery -= math.sin(math.radians(self.dir)) * self.speed
+
+    def draw_E(self):   # 공을 circle이 아니라 ellipse로 생성
+        pygame.draw.ellipse(Surface, self.col, self.rect)
+
+    def draw_R(self):
+        pygame.draw.rect(Surface, self.col, self.rect)
+
 pygame.init()
 Surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 FPSLOCK = pygame.time.Clock()
@@ -20,11 +39,16 @@ def main():
     is_game_start = False
     score = 0
     BLOCK = []
+    BALL = Block((200,200,0),Rect(375, 650, 20, 20), 10)
     # 클래스 생성
     # 무지개색 정보
     colors=[(255, 0, 0), (255, 150, 0), (255,228,0), 
             (11,201,4), (0, 80, 255), (0, 0, 147),
             (201, 0, 167)]
+    # 초기에 생성될 블럭들(무지개색으로 아홉개씩, 총 54개 블록)
+    for y, color in enumerate(colors, start=0): # y값은 0 ~ 6
+        for x in range(0, 9): 
+            BLOCK.append(Block(color, Rect(x*80 + 150, y*40 + 40, 60, 20)))
     
     bigFont = pygame.font.SysFont('NanumGothic', 80)
     smallFont = pygame.font.SysFont('NanumGothic', 45)
@@ -34,6 +58,7 @@ def main():
     M_FAIL = bigFont.render('FAILED', True, 'red')
 
     while True:
+        # 스코어, 스피드 글자.
         Surface.fill((0,0,0))
         for event in pygame.event.get():    # 이벤트처리 기본 
             if event.type == QUIT:  
@@ -54,8 +79,23 @@ def main():
             Surface.blit(M_GAME_SUBTITLE, ((SCREEN_WIDTH/2)-(300/2),
                                         (SCREEN_HEIGHT/2)+50))
         else:   # 게임시작 후 블록을 다 그리고 볼이 움직이게 처리, 바도 움직이도록
-            Surface.blit(M_CLEAR, (80,280))
+            
+            LenBlock = len(BLOCK)   # 54으로 시작
+            # BLOCK = [x for x in BLOCK]
 
+            if BALL.rect.centery < 1000:
+                BALL.move()
+
+            if BALL.rect.centerx < 0 or BALL.rect.centerx > 1000:   # 게임화면 양쪽 벽 밖으로 못나가게
+                BALL.dir = 180 - BALL.dir   # 반사각만큼 방향 전화
+            elif BALL.rect.centery < 0 :    ## 게임 화면 천장에 부딪히면 반사
+                BALL.dir = -BALL.dir
+
+            BALL.draw_E()
+
+            for i in BLOCK: # Block()
+                i.draw_R()               
+                   
         pygame.display.update() 
         FPSLOCK.tick(30)    
 
